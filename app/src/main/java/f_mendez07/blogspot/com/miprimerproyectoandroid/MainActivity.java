b2280d3abdd45cb549871b4363fd9c14651372a5
FJavier117 <fjmendez07@gmail.com>
2020-01-19 00:46:08 -0600

    primer commit

---
+++ app/src/main/java/f_mendez07/blogspot/com/miprimerproyectoandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package f_mendez07.blogspot.com.miprimerproyectoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
